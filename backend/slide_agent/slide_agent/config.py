4093359aa949510a880f9550148b24e9b9300974
Johnson <linghuchongxajh@qq.com>
2025-07-04 11:37:04 +0800

    2025-07-04_11:37

--- backend/slide_agent/slide_agent/config.py
+++ backend/slide_agent/slide_agent/config.py
@@ -8,24 +8,28 @@
 
 ##每个Agent的使用的模型配置
 SPLIT_TOPIC_AGENT_CONFIG = {
-    "provider": "google",
-    "model": "gemini-2.0-flash",
+    # "provider": "google",
+    # "model": "gemini-2.0-flash",
+    "provider": "deepseek",
+    "model": "deepseek-chat",
 }
 
 TOPIC_RESEARCH_AGENT_CONFIG = {
-    "provider": "google",
-    "model": "gemini-2.0-flash",
-    # "provider": "deepseek",
+    # "provider": "google",
+    # "model": "gemini-2.0-flash",
+    "provider": "deepseek",
     # "model": "gpt-4.1",
     # "model": "gpt-4.1-nano-2025-04-14",
-    # "model": "deepseek-chat",
+    "model": "deepseek-chat",
 }
 SUMMARY_AGENT_CONFIG = {
-    "provider": "google",
-    "model": "gemini-2.0-flash",
+    # "provider": "google",
+    # "model": "gemini-2.0-flash",
     # "provider": "openai",
     # "model": "gpt-4.1",
     # "model": "gemini-2.0-flash",
     # "provider": "local_ali",
     # "model": "qwen-turbo-latest",
+    "provider": "deepseek",
+    "model": "deepseek-chat",
 }
